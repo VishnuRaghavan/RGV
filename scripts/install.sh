6248aa1eaf8ce1dc78d63e6c9635c05470163334
VishnuRaghvan <vishnuraghav16@gmail.com>
2021-02-20 16:38:47 +0400

    done: yum install added.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
+# install yum
+sudo apt-get update -y
+sudo apt-get install -y yum
+
 # update instance
-# sudo yum -y update
-sudo apt-get update
+sudo yum -y update
 
 # add nodejs to yum
 curl --silent --location https://rpm.nodesource.com/setup_12.x | bash -
-# sudo yum -y install nodejs
-sudo apt install nodejs
-sudo apt install npm
+sudo yum -y install nodejs
 
 # install pm2 module globaly
 npm install -g pm2
